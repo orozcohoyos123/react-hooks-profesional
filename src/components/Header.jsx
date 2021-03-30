fff0499dc2c24b8cc8ebb8c6788801b1cce723cb
unknown <orozcohoyos123@gmail.com}>
2021-03-29 19:00:58 -0500

    useState

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const Header = () => {
+  const [darkmode, setDarkMode] = useState(false);
+  
+  const handleClick = () => {
+    setDarkMode(!darkmode);
+  }
+
+  return (
+    <div className="Header">
+      <h1>React Hooks</h1>
+      <button type="button" onClick={handleClick}>{darkmode ? 'Dark Mode' : 'Light Mode'}</button>
+    </div>
+  );
+};
+
+export default Header;
