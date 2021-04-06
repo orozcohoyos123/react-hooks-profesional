61e248e7b9649d44bd0d4b709d56b3c70211f459
sorozco <orozcohoyos123@gmail.com>
2021-04-05 22:17:09 -0500

    useReducer, para implementar cambios de estado para implementar la adicion de personajes favoritos, similar a redux

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,8 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import './styles/Characters.css';
+
+const initialState = {
+  favorites: []
+}
+
+const favoriteReducer = (state, action) => {
+  switch(action.type){
+    case 'ADD_TO_FAVORITE':
+      if (state.favorites.filter((favorite) => favorite.id === action.payload.id).length !== 0) {
+        return state;
+      }
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload]
+      }
+    case 'REMOVE_FROM_FAVORITE':
+      return {
+        ...state,
+        favorites: state.favorites.filter(favorite => favorite.id !== action.payload)
+      }
+    default: 
+      return state;
+  }
+}
 
 const Characters = (props) => {
   const [characters, setCharacters] = useState([]);
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character/")
@@ -10,18 +35,48 @@ const Characters = (props) => {
       .then((data) => setCharacters(data.results));
   }, []);
 
+  const handleClick = favorite => {
+    dispatch(
+      {type: 'ADD_TO_FAVORITE', payload: favorite}
+    )
+  }
+
+  const handleRemoveClick = favorite => {
+    dispatch(
+      {type: 'REMOVE_FROM_FAVORITE', payload: favorite}
+    )
+  }
+  
   return (
-    <div className="Character">
-      {characters.map((character) => (
-        <div key={character.id} className="Character__container">
-          <img src={character.image} alt={character.name} />
-          <div className="Character__info">
-            <label className="Character__title">{character.name}</label>
-            <label className="Character__status">{character.status}</label>
-            <label className="Character__origin">{character.origin.name}</label>
+    <div className="Container">
+      <h3>Favorites Characters</h3>
+      <div className="Character Separator">
+        {favorites.favorites.map((character) => (
+          <div key={character.id} className="Character__container">
+            <img src={character.image} alt={character.name} />
+            <div className="Character__info">
+              <label className="Character__title">{character.name}</label>
+              <label className="Character__status">{character.status}</label>
+              <label className="Character__origin">{character.origin.name}</label>
+            </div>
+            <button type="button" onClick={() => handleRemoveClick(character.id)}>Remove from favorites</button>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <hr/>
+      <div className="Character">
+        {characters.map((character) => (
+          <div key={character.id} className="Character__container">
+            <img src={character.image} alt={character.name} />
+            <div className="Character__info">
+              <label className="Character__title">{character.name}</label>
+              <label className="Character__status">{character.status}</label>
+              <label className="Character__origin">{character.origin.name}</label>
+            </div>
+            <button type="button" onClick={() => handleClick(character)}>Add to favorites</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
